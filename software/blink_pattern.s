7727f1c6374263ff67f0a49b31175eb5ecd4169f
fineboom1000 <fineboom1000@gmail.com>
2025-12-19 10:20:25 -0500

    final days

---
+++ software/blink_pattern.s
@@ -0,0 +1,32 @@
+.section .text.startup
+.global _start
+
+_start:
+    lui  a0, 0x80000
+    li   a1, 0x01
+    sw   a1, 0(a0)
+    li   t0, 0x100000
+delay1:
+    addi t0, t0, -1
+    bnez t0, delay1
+    li   a1, 0x02
+    sw   a1, 0(a0)
+    li   t0, 0x100000
+delay2:
+    addi t0, t0, -1
+    bnez t0, delay2
+    li   a1, 0x04
+    sw   a1, 0(a0)
+    li   t0, 0x100000
+delay3:
+    addi t0, t0, -1
+    bnez t0, delay3
+    li   a1, 0x08
+    sw   a1, 0(a0)
+    li   t0, 0x100000
+delay4:
+    addi t0, t0, -1
+    bnez t0, delay4
+    li   a1, 0x0F
+    sw   a1, 0(a0)
+    j    _start
